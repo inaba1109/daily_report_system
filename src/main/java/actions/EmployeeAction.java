0a4e3e4af81ce014ae8c08619066132ae72e1aa1
ryoutarou.ishiguro <inaba110966@gmail.com>
2023-02-15 20:45:11 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -32,6 +32,22 @@ public class EmployeeAction extends ActionBase {
 
         service.close();
     }
+    /**
+     * 新規登録画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void entryNew() throws ServletException, IOException {
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
+    }
+
+
+
 
     /**
      * 一覧画面を表示する
@@ -61,6 +77,9 @@ public class EmployeeAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
+
+
+
 
     }
 
